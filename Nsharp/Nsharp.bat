3fdbff24e094a56795615d942720874b6b8b36ad
Bray1234code <125764860+Bray1234code@users.noreply.github.com>
2023-02-18 14:32:38 -0500

    Add files via upload

---
+++ Nsharp/Nsharp.bat
@@ -0,0 +1,114 @@
+:: Nasharp scripting launguage is an expansion pack for bash scripting
+:: When using nsharp code if you are fimilar with python such as print and
+:: stuff this expansion
+
+@echo off
+
+:: This will print stuff to the screen
+
+set print=echo
+set Print=echo
+
+:: This will clear the screen
+
+set clear=cls
+set Clear=cls
+
+:: This will end your script
+
+set stop=pause
+set Stop=pause
+
+:: This will end the programe
+
+set end=exit
+set End=exit
+
+:: This will put call a file
+
+set include=call
+set Include=call
+
+:: This will read a file and put it on the screen
+
+set import=type
+set Import=type
+
+:: This will goto varbiles
+
+set return=goto
+set Return=goto
+
+:: This will also goto a varblies
+
+set goto=goto
+set Goto=goto
+
+:: This will deleate a file
+
+set remove=del
+set Remove=del
+
+:: This will create varibles
+
+set int=set
+set Int=set
+
+:: This will create an if statement
+
+set if=if
+set If=if
+
+:: This will get imput
+
+set imput=set /p
+set Imput=set /p
+
+:: This will change the dir
+
+set dir=chdir
+set Dir=chdir
+
+:: This will goto the previos
+
+set pd=cd\
+set Pd=cd\
+
+:: This will make a class
+
+set class=set class_
+set Class=set class_
+
+:: This will call javascript files
+
+set js=Node.js
+set Js=Node.js
+
+:: This will alow you to do comments
+
+set nn=rem
+set NN=rem
+set nN=rem
+set Nn=rem
+
+:: This can create varibles
+
+set var=set
+Set Var=set
+
+:: This can make you be able to change the colors
+
+set col=color
+set Col=color
+
+:: This will make an empty space
+
+set space=echo.
+set Space=echo.
+
+:: This will do math
+
+set math=set /a
+set Math=set /a
+
+
